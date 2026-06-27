b922b96d05aee31494f257ddc5764a37fdb57883
oleg <oleg_09-95@mail.ru>
2026-06-27 15:57:18 +0300

    17 cart badge with computed signals

--- src/app/cart/cart-service.ts
+++ src/app/cart/cart-service.ts
@@ -1,14 +1,24 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Product } from '../products/product';
+import { CartItem } from './cart-item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  private readonly cartItems = signal<Product[]>([]);
+  private readonly cartItems = signal<CartItem[]>([]);
+
+  readonly totalItems = computed(
+    () => this.cartItems()
+      .reduce((total, item) => total + item.quantity, 0))
 
   addToCart(product: Product) {
-    console.log(product);
-    this.cartItems.update((prevProducts) => [...prevProducts, product]);
+    this.cartItems.update(prevItems => {
+      const existingItem = prevItems.find(item => item.product.id === product.id);
+      if (existingItem) {
+        return  prevItems.map(item => item.product.id === product.id ? {...item, quantity: item.quantity + 1} : item);
+      }
+      return [...prevItems, {quantity: 1, product}];
+    })
   }
 }
